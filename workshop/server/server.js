aa64cef3764c3490d1e304a723be515a7413f7de
Keefer Rourke <mail@krourke.org>
2019-10-25 11:09:48 -0400

    Format and change routes

--- workshop/server/server.js
+++ workshop/server/server.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/1337h4x0r', (req, res) => res.download('./1.tar'))
-app.get('/1337h4x0r69', (req, res) => res.download('./2.tar'))
-app.get('/1337420h4x0r', (req, res) => res.download('./3.tar'))
+app.get("/1337h4x0r", (req, res) => {
+    return res.download("./1.tar");
+});
+app.get("/1337h4x0r2", (req, res) => {
+    return res.download("./2.tar");
+});
+app.get("/1337h4x0r3", (req, res) => {
+    return res.download("./3.tar");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    return console.log(`File server listening on port ${port}!`);
+});
